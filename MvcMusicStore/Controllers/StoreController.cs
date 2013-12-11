4d13ad5287107b344e2f75b5c8f55118e6dcdf44
Yggy <igor.yentaltsev@gmail.com>
2013-12-11 17:04:50 -0500

    Added 2 action methods

--- MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -11,10 +11,19 @@ namespace MvcMusicStore.Controllers
         //
         // GET: /Store/
 
-        public ActionResult Index()
+        public string Index()
         {
-            return View();
+            return "Hello from Store.Index()";
         }
 
+        public string Browse()
+        {
+            return "Hello from Store.Browse()";
+        }
+
+        public string Details()
+        {
+            return "Hello from Store.Details()";
+        }
     }
 }
