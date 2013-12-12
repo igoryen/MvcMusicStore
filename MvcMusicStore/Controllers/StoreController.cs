90668e618e743c347f158d3f0eb1d9743c2b1cfe
Yggy <igor.yentaltsev@gmail.com>
2013-12-12 14:15:18 -0500

    Added a namespace StoreController

--- MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcMusicStore.Models;
 
 namespace MvcMusicStore.Controllers {
   public class StoreController : Controller {
