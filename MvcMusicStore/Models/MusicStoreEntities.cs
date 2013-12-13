2cfb0055b4fc499aaf3e5bfad0f1978de550b91b
Yggy <igor.yentaltsev@gmail.com>
2013-12-12 20:16:18 -0500

    Added StoreManager controller

--- MvcMusicStore/Models/MusicStoreEntities.cs
+++ MvcMusicStore/Models/MusicStoreEntities.cs
@@ -8,5 +8,7 @@ namespace MvcMusicStore.Models {
   public class MusicStoreEntities : DbContext {
     public DbSet<Album> Albums { get; set; }
     public DbSet<Genre> Genres { get; set; }
+
+    public DbSet<Artist> Artists { get; set; }
   }
 }
